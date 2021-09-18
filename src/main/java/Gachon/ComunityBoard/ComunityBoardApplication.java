bfcb95356b7a286455522cdf0994cb81e61ee151
SeungHwan <vw9801@naver.com>
2021-09-18 23:52:39 +0900

    Initial commit

---
+++ src/main/java/Gachon/ComunityBoard/ComunityBoardApplication.java
@@ -0,0 +1,13 @@
+package Gachon.ComunityBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunityBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunityBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Gachon/ComunityBoard/ComunityBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package Gachon.ComunityBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunityBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
