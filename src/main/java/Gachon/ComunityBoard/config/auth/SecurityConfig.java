a0f3554dd2f27dfb1c05485bce4df517a31ec77a
SeungHwan <vw9801@naver.com>
2021-10-07 23:58:01 +0900

    전체조회,전체조회(페이징),게시물검색만 비로그인에 허용

--- src/main/java/Gachon/ComunityBoard/config/auth/SecurityConfig.java
+++ src/main/java/Gachon/ComunityBoard/config/auth/SecurityConfig.java
@@ -19,11 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/api/board","/","/css/**","/js/**","/images/**","/board").permitAll()// 모두에게 허용
+                    .antMatchers("/api/board","/","/css/**","/js/**","/images/**","/api/board/search/**","/api/board/all").permitAll()// 모두에게 허용
 //                    .antMatchers("/api/**").hasRole(Role.USER.name()) // User권한가진사람한테만 허용
 //                    .anyRequest().authenticated() //나머지페이지는 인증된 사람에게만 허용
-                    .antMatchers("/api/board/posts/**","/api/user/userInfo/**").authenticated()
-                    .anyRequest().permitAll()
+                    //.antMatchers("/api/board/posts/**","/api/user/userInfo/**").authenticated()
+                    .anyRequest().authenticated()
                 .and()
                     .logout()
                         .logoutSuccessUrl("/api/board")
