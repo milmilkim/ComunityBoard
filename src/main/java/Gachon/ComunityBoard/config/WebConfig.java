3e664f2685c5891ca06ae97573b68bd54113586a
SeungHwan <vw9801@naver.com>
2021-10-09 02:16:21 +0900

    cors 임시해결책

--- src/main/java/Gachon/ComunityBoard/config/WebConfig.java
+++ src/main/java/Gachon/ComunityBoard/config/WebConfig.java
@@ -5,6 +5,7 @@ import Gachon.ComunityBoard.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,6 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    //cors문제 임시대책
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:3000");
     }
 
 
